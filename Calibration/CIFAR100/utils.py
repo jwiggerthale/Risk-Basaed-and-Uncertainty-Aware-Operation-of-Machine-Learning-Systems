20f266049133aea75ba607bf01312a31c563ee5c
jwiggerthale <155433120+jwiggerthale@users.noreply.github.com>
2026-03-16 14:11:22 +0100

    Add seed setting utility for reproducibility
    This script sets a random seed for reproducibility in experiments.

---
+++ Calibration/CIFAR100/utils.py
@@ -0,0 +1,15 @@
+'''
+This script implements setting of seed
+'''
+
+import torch
+import numpy as np
+import random
+
+
+def set_seed(seed=42):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
