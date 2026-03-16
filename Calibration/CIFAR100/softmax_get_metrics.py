af0d559932cb087aba92ed94a8b9e90ef2a48700
jwiggerthale <155433120+jwiggerthale@users.noreply.github.com>
2026-03-16 14:07:34 +0100

    Create softmax_get_metrics.py

---
+++ Calibration/CIFAR100/softmax_get_metrics.py
@@ -0,0 +1,524 @@
+
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torchvision import transforms
+import numpy as np
+import os
+from modules.cifar_data_utils import image_dataset, transform_test, image_dataset_c, get_cifar_files, class_map, convert_to_c_class, convert_from_c_class
+from modules.metrics import *
+from modules.base_models import resnet18, efficientnet_b0
+import json
+import argparse
+from torchvision import transforms
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "--model",
+    default="resnet18",
+    help="Pick an architecture (default: ResNet18)",
+)
+
+parser.add_argument(
+    "--out_dir",
+    default="./ensemble_resnet",
+    help="Where to store (default: ensemble_resnet)",
+)
+
+parser.add_argument(
+    "--save_dir",
+    default="./mc/balanced_resnet18",
+    help="Parent dir where dirs for each ensemble are loacted",
+)
+
+args = parser.parse_args()
+
+
+
+# get data loader
+train_files, val_files, test_files = get_cifar_files()
+
+
+test_set = image_dataset(image_files=test_files, 
+                          mappings = class_map, 
+                          transforms=transform_test)
+
+test_loader = DataLoader(test_set, 
+                        batch_size=64, 
+                        shuffle=False, 
+                        drop_last = True)
+
+
+c_dir = './CIFAR-100-C'
+files = [f'{c_dir}/{f}' for f in os.listdir(c_dir) if f.endswith('.npy') and f != 'labels.npy']
+
+label_file = f'{c_dir}/labels.npy'
+labels = np.load(label_file)
+c_sets = {}
+for fp in files:
+        ims = np.load(fp)
+        for severity in np.arange(1, 6):
+            start = (severity-1)*10000
+            end = severity * 10000
+            c_set = image_dataset_c(ims = ims[start:end], 
+                                    labels=labels[start:end], 
+                                    transforms=  transforms.Compose([transforms.ToTensor()]))
+            c_loader = DataLoader(c_set, 
+                            batch_size=16, 
+                            shuffle=False, 
+                            drop_last = True)
+            key = fp.split('/')[-1].split('.')[0]
+            key = f'{key}_sev_{severity}'
+            c_sets[key]  = c_loader
+
+
+'''
+import matplotlib.pyplot as plt
+
+all_ims = {}
+for key, dl in c_sets.items():
+    if 'sev_1' in key:
+        ims, _ = next(iter(dl))
+        all_ims[key.split('_')[0]] =ims[0].permute(1,2,0)
+
+use_keys = []
+
+fig, axes = plt.subplots(2,3, figsize = (30, 20))
+axes[0][0].imshow(all_ims[use_keys[0]])
+axes[0][0].set_title(use_keys[0], fontsize = 18)
+axes[0][0].axis('off')
+axes[0][1].imshow(all_ims[use_keys[1]])
+axes[0][1].set_title(use_keys[1], fontsize = 18)
+axes[0][1].axis('off')
+axes[0][2].imshow(all_ims[use_keys[2]])
+axes[0][2].set_title(use_keys[2], fontsize = 18)
+axes[0][2].axis('off')
+axes[1][0].imshow(all_ims[use_keys[3]])
+axes[1][0].set_title(use_keys[3], fontsize = 18)
+axes[1][0].axis('off')
+axes[1][1].imshow(all_ims[use_keys[4]])
+axes[1][1].set_title(use_keys[4], fontsize = 18)
+axes[1][1].axis('off')
+axes[1][2].imshow(all_ims[use_keys[5]])
+axes[1][2].set_title(use_keys[5], fontsize = 18)
+axes[1][2].axis('off')
+plt.tight_layout()
+plt.savefig('CIFAR_C_Example.png')
+'''
+
+
+# get model and load weights
+if 'resnet' in args.model.lower():
+    model = resnet18(num_classes=100)
+else:
+    model = efficientnet_b0(num_classes=100)
+    
+
+model.load_state_dict(torch.load(args.save_dir))
+model.eval()
+model.cuda()
+
+
+
+# get predictions and metrics per dataset
+preds = []
+labels = []
+eus = []
+sigmas = []
+pred_cls = []
+uncertainties = []
+for x, y, _ in iter(test_loader):
+    x = x.cuda()
+    mu, sigma = model.forward(x)
+    preds.extend(torch.softmax(mu, dim=-1).detach().cpu().tolist())
+    pred = mu.argmax(dim = 1)
+    labels.extend(y.tolist())
+    pred_cls.extend(pred.cpu().detach().tolist())   
+    entropy = -(mu.clamp_min(1e-8) * mu.clamp_min(1e-8).log()).sum(dim=1)
+    uncertainties.extend(entropy.cpu().detach().numpy())
+
+
+uncertainties = np.array(uncertainties)
+
+model_stats = {}
+
+acc = accuracy(preds = preds, 
+               labels = labels)
+
+f1 = get_f1_score(preds=preds, 
+                  labels = labels)
+
+model_stats['acc'] = acc
+model_stats['f1'] = f1
+
+
+ece_5 = expected_calibration_error(samples = preds, 
+                                 true_labels=labels, 
+                                 M = 5)
+
+
+ece_10 = expected_calibration_error(samples = preds, 
+                                 true_labels=labels, 
+                                 M = 10)
+
+ece_15 = expected_calibration_error(samples = preds, 
+                                 true_labels=labels, 
+                                 M = 15)
+
+ece_20 = expected_calibration_error(samples = preds, 
+                                 true_labels=labels, 
+                                 M = 20)
+
+ece_40 = expected_calibration_error(samples = preds, 
+                                 true_labels=labels, 
+                                 M = 40)
+
+skce_dict = p_value_skce_ul(probs = preds, 
+                            labels = labels)
+
+
+aurc, coverage, risk = compute_aurc(y_true = labels, 
+                    y_pred = pred_cls, 
+                    uncertainty=uncertainties)
+aurc_eu = 0
+aurc_au = 0
+nll = compute_nll_multiclass(y_true = np.array(labels), probs = np.array(preds))
+
+
+skce = skce_dict['SKCE_ul']
+p_value = skce_dict['p_value']
+model_stats['ece_5'] = ece_5
+model_stats['ece_10'] = ece_10
+model_stats['ece_15'] = ece_15
+model_stats['ece_20'] = ece_20
+model_stats['ece_40'] = ece_40
+model_stats['skce'] = skce
+model_stats['skce_p'] = p_value
+model_stats['aurc'] = aurc
+model_stats['aurc_eu'] = aurc_eu
+model_stats['aurc_au'] = aurc_au
+model_stats['nll'] = nll
+
+
+
+if not os.path.isdir(args.out_dir):
+    os.makedirs(args.out_dir, exist_ok=True)
+
+with open (f'{args.out_dir}/model_stats_test.json', 'w', encoding = 'utf-8') as out_file:
+    json.dump(model_stats, out_file)
+
+
+plot_aggregated_calibration_curve(probs = preds, 
+                                labels = labels, 
+                                n_bins=20,  
+                                save_path = f'{args.out_dir}/{args.model}_calibration_curve_test.png')
+
+
+fractions = np.arange(0.5, 1.01, 0.1)
+abstained_results = abstained_prediction(y_true = labels, 
+                                         y_uncertainty=uncertainties, 
+                                         probs=preds, 
+                                         fractions = fractions)
+
+
+
+abstained_results ={'ResNet18': abstained_results}
+plot_abstained_prediction(results = abstained_results, 
+                          save_path=f'{args.out_dir}/{args.model}_abstained_prediction_curve_test.png')
+
+
+
+# get rersults for corrupted data by severity
+c_preds = []
+c_labels = []
+c_pred_cls = []
+c_uncs = []
+for severity in np.arange(1, 6):
+    all_preds = []
+    all_labels = []
+    all_uncs = []
+    all_pred_cls = []
+    for key, dl in c_sets.items():
+        if f'sev_{severity}' in key:
+            print(f'Using key: {key}')
+            preds = []
+            labels = []
+            eus = []
+            sigmas = []
+            pred_cls = []
+            uncertainties = []
+            for x, y in iter(dl):
+                x = x.cuda()
+                mu, sigma = model.forward(x)
+                preds.extend(torch.softmax(mu, dim=-1).detach().cpu().tolist())
+                all_preds.extend(torch.softmax(mu, dim=-1).detach().cpu().tolist())
+                c_preds.extend(torch.softmax(mu, dim=-1).detach().cpu().tolist())
+                entropy = -(mu.clamp_min(1e-8) * mu.clamp_min(1e-8).log()).sum(dim=1)
+                pred = mu.argmax(dim = 1)
+                pred_cls.extend(pred.cpu().detach().tolist())
+                c_pred_cls.extend(pred.cpu().detach().tolist())
+                all_pred_cls.extend(pred.cpu().detach().tolist())
+                new_labels = y.tolist()
+                new_labels = [convert_from_c_class[elem] for elem in new_labels]
+                labels.extend(new_labels)
+                c_labels.extend(new_labels)
+                all_labels.extend(new_labels)
+                uncertainties.extend(entropy.cpu().detach().numpy())
+
+            all_uncs.extend(uncertainties)
+            c_uncs.extend(uncertainties)
+            model_stats = {}
+            acc = accuracy(preds = preds, 
+                        labels = labels)
+            f1 = get_f1_score(preds=preds, 
+                            labels = labels)
+            model_stats['acc'] = acc
+            model_stats['f1'] = f1
+
+            # uncomment this code if you also need results per augmentation type
+            '''
+
+            ece_5 = expected_calibration_error(samples = preds, 
+                                            true_labels=labels, 
+                                            M = 5)
+
+
+            ece_10 = expected_calibration_error(samples = preds, 
+                                            true_labels=labels, 
+                                            M = 10)
+
+            ece_15 = expected_calibration_error(samples = preds, 
+                                            true_labels=labels, 
+                                            M = 15)
+
+            ece_20 = expected_calibration_error(samples = preds, 
+                                            true_labels=labels, 
+                                            M = 20)
+
+            ece_40 = expected_calibration_error(samples = preds, 
+                                            true_labels=labels, 
+                                            M = 40)
+
+            skce_dict = p_value_skce_ul(probs = preds, 
+                                        labels = labels)
+            skce = skce_dict['SKCE_ul']
+            p_value = skce_dict['p_value']
+            model_stats['ece_5'] = ece_5
+            model_stats['ece_10'] = ece_10
+            model_stats['ece_15'] = ece_15
+            model_stats['ece_20'] = ece_20
+            model_stats['ece_40'] = ece_40
+            model_stats['skce'] = skce
+            model_stats['skce_p'] = p_value
+
+
+            if not os.path.isdir(args.out_dir):
+                os.makedirs(args.out_dir, exist_ok=True)
+
+            with open (f'{args.out_dir}/model_stats_{key}.json', 'w', encoding = 'utf-8') as out_file:
+                json.dump(model_stats, out_file)
+
+
+            plot_aggregated_calibration_curve(probs = conf_vals, 
+                                            labels = labels, 
+                                            n_bins=20, 
+                                            save_path = f'{args.out_dir}/{args.model}_calibration_curve_{key}.png')
+
+
+            fractions = np.arange(0.5, 1.01, 0.1)
+            abstained_results = abstained_prediction(y_true = labels, 
+                                                    y_uncertainty=uncertainties, 
+                                                    probs=preds, 
+                                                    fractions = fractions)
+
+
+            abstained_results ={'EDL': abstained_results}
+            plot_abstained_prediction(results = abstained_results, 
+                                    save_path=f'{args.out_dir}/{args.model}_abstained_prediction_curve_{key}.png')
+            '''
+        else:
+            print(f'Skipping key: {key}')
+
+    model_stats = {}
+    acc = accuracy(preds = all_preds, 
+                labels = all_labels)
+    f1 = get_f1_score(preds=all_preds, 
+                    labels = all_labels)
+    model_stats['acc'] = acc
+    model_stats['f1'] = f1
+    uncertainties = np.array(all_uncs)
+
+    ece_5 = expected_calibration_error(samples = all_preds, 
+                                    true_labels=all_labels, 
+                                    M = 5)
+
+
+    ece_10 = expected_calibration_error(samples = all_preds, 
+                                    true_labels=all_labels, 
+                                    M = 10)
+
+    ece_15 = expected_calibration_error(samples = all_preds, 
+                                    true_labels=all_labels, 
+                                    M = 15)
+
+    ece_20 = expected_calibration_error(samples = all_preds, 
+                                    true_labels=all_labels, 
+                                    M = 20)
+
+    ece_40 = expected_calibration_error(samples = all_preds, 
+                                    true_labels=all_labels, 
+                                    M = 40)
+
+    skce_dict = p_value_skce_ul(probs = all_preds, 
+                                    labels=all_labels)
+
+
+    aurc, coverage, risk = compute_aurc(y_true = all_labels, 
+                    y_pred = all_pred_cls, 
+                    uncertainty=all_uncs)
+    nll = compute_nll_multiclass(y_true = np.array(all_labels), probs = np.array(all_preds))
+
+
+    skce = skce_dict['SKCE_ul']
+    p_value = skce_dict['p_value']
+    model_stats['ece_5'] = ece_5
+    model_stats['ece_10'] = ece_10
+    model_stats['ece_15'] = ece_15
+    model_stats['ece_20'] = ece_20
+    model_stats['ece_40'] = ece_40
+    model_stats['skce'] = skce
+    model_stats['skce_p'] = p_value
+    model_stats['aurc'] = aurc
+    model_stats['aurc_eu'] = aurc_eu
+    model_stats['aurc_au'] = aurc_au
+    model_stats['nll'] = nll
+
+
+
+    if not os.path.isdir(args.out_dir):
+        os.makedirs(args.out_dir, exist_ok=True)
+
+    with open (f'{args.out_dir}/model_stats_c_combined_sev_{severity}.json', 'w', encoding = 'utf-8') as out_file:
+        json.dump(model_stats, out_file)
+
+
+
+
+    plot_aggregated_calibration_curve(probs = np.array(all_preds), 
+                                    labels = all_labels, 
+                                    n_bins=20, 
+                                    save_path = f'{args.out_dir}/{args.model}_calibration_curve_c_combined_sev_{severity}.png')
+
+
+    fractions = np.arange(0.5, 1.01, 0.1)
+    abstained_results = abstained_prediction(y_true = all_labels, 
+                                            y_uncertainty=all_uncs, 
+                                            probs=all_preds,  
+                                            fractions = fractions)
+
+
+    abstained_results ={'Softmax': abstained_results}
+    plot_abstained_prediction(results = abstained_results, 
+                            save_path=f'{args.out_dir}/{args.model}_abstained_prediction_curve_c_combined_sev_{severity}.png')
+    
+
+
+
+# evaluate results for all severities combined
+model_stats = {}
+
+acc = accuracy(preds = c_preds, 
+            labels = c_labels)
+
+f1 = get_f1_score(preds=c_preds, 
+                labels = c_labels)
+
+model_stats['acc'] = acc
+model_stats['f1'] = f1
+
+uncertainties = np.array(c_uncs)
+
+
+
+ece_5 = expected_calibration_error(samples = c_preds, 
+                                true_labels=c_labels, 
+                                M = 5)
+
+
+ece_10 = expected_calibration_error(samples = c_preds, 
+                                true_labels=c_labels, 
+                                M = 10)
+
+ece_15 = expected_calibration_error(samples = c_preds, 
+                                true_labels=c_labels, 
+                                M = 15)
+
+ece_20 = expected_calibration_error(samples = c_preds, 
+                                true_labels=c_labels, 
+                                M = 20)
+
+ece_40 = expected_calibration_error(samples = c_preds, 
+                                true_labels=c_labels, 
+                                M = 40)
+
+skce_dict = p_value_skce_ul(probs = c_preds, 
+                                labels=c_labels)
+
+
+aurc, coverage, risk = compute_aurc(y_true = c_labels, 
+                    y_pred = c_pred_cls, 
+                    uncertainty=c_uncs)
+aurc_eu = 0
+aurc_au = 0
+nll = compute_nll_multiclass(y_true = np.array(c_labels), probs = np.array(c_preds))
+
+
+skce = skce_dict['SKCE_ul']
+p_value = skce_dict['p_value']
+model_stats['ece_5'] = ece_5
+model_stats['ece_10'] = ece_10
+model_stats['ece_15'] = ece_15
+model_stats['ece_20'] = ece_20
+model_stats['ece_40'] = ece_40
+model_stats['skce'] = skce
+model_stats['skce_p'] = p_value
+model_stats['aurc'] = aurc
+model_stats['aurc_eu'] = aurc_eu
+model_stats['aurc_au'] = aurc_au
+model_stats['nll'] = nll
+
+
+
+
+
+if not os.path.isdir(args.out_dir):
+    os.makedirs(args.out_dir, exist_ok=True)
+
+with open (f'{args.out_dir}/model_stats_c_combined_sev_all.json', 'w', encoding = 'utf-8') as out_file:
+    json.dump(model_stats, out_file)
+
+
+
+
+plot_aggregated_calibration_curve(probs = np.array(c_preds), 
+                                labels = c_labels, 
+                                n_bins=20, 
+                                save_path = f'{args.out_dir}/{args.model}_calibration_curve_c_combined_sev_all.png')
+
+
+fractions = np.arange(0.5, 1.01, 0.1)
+abstained_results = abstained_prediction(y_true = c_labels, 
+                                        y_uncertainty=uncertainties, 
+                                        probs = c_preds, 
+                                        fractions = fractions)
+
+
+abstained_results ={'EDL': abstained_results}
+plot_abstained_prediction(results = abstained_results, 
+                        save_path=f'{args.out_dir}/{args.model}_abstained_prediction_curve_c_combined_sev_all.png')
+
+
+
