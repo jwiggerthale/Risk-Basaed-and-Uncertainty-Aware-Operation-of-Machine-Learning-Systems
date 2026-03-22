d10958fe9c3948720ae0acafdabf7dc079d1416e
jwiggerthale <155433120+jwiggerthale@users.noreply.github.com>
2026-03-22 08:38:46 +0100

    Create utils.py

---
+++ Risk/Diabetes/modules/utils.py
@@ -0,0 +1,15 @@
+'''
+Function for logging
+'''
+
+import datetime
+
+
+def log(message: str, 
+        file: str = 'log_file.txt', 
+        ):
+    now = datetime.datetime.now()
+    with open(file, 'a') as out_file:
+        out_file.write(f'{now}\t{message}')
+        out_file.write('\n')
+
