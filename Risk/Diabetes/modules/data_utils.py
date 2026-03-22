3a7f1eaa3680ee4d0c13410f790cbc6f756b1595
jwiggerthale <155433120+jwiggerthale@users.noreply.github.com>
2026-03-22 08:50:27 +0100

    Create data_utils.py

---
+++ Risk/Diabetes/modules/data_utils.py
@@ -0,0 +1,21 @@
+'''
+This script implements a simple torch data loader from tabular data
+'''
+
+from torch.utils.data import Dataset
+
+
+
+class heart_ds(Dataset):
+    def __init__(self, 
+                 x: list, 
+                 y: list):
+        self.x = x
+        self.y = y
+    def __len__(self):
+        return len(self.x)
+    def __getitem__(self, 
+                  idx: int):
+        x = self.x[idx]
+        y = self.y[idx]
+        return x, y
