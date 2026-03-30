3ce1d2d739f7f48036e6179badf8c7a19f8df8d0
jwiggerthale <155433120+jwiggerthale@users.noreply.github.com>
2026-03-30 14:32:15 +0200

    Add evaluation model for CIFAR-100 in SMILE framework
    Dummy

---
+++ SMILE/CIFAR/evaluate_model.py
@@ -0,0 +1,5 @@
+'''
+This file implements evaluation of models trained on the CIFAR-100 dataset accoring to the SMILE frmaework
+Note that it is based on simple ensemble models as trained for the experiments conducted in Chapters 5 and 6 of the thesis
+No implementation of specific training scripts etc. is therefore necessary
+'''
